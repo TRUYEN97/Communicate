1d325cb6de6849ae4e6278dd30c24564e272e5ae
SQT <samtruyen60@mail.com>
2022-08-30 19:59:40 +0700

    fix loi khong the gui request qua 2 lan

--- src/Communicate/PowerSwitch/PowerSwitch.java
+++ src/Communicate/PowerSwitch/PowerSwitch.java
@@ -9,7 +9,6 @@ import java.nio.charset.StandardCharsets;
 import org.apache.commons.codec.binary.Base64;
 import org.apache.http.HttpHeaders;
 import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.HttpClientBuilder;
 
@@ -19,23 +18,21 @@ import org.apache.http.impl.client.HttpClientBuilder;
  */
 public class PowerSwitch {
 
-    private final HttpClient httpclient;
     private final String authHeader;
     private final String host;
     private final StringBuilder result;
 
     public PowerSwitch(String host, String username, String password) {
         this.host = host;
-        this.httpclient = HttpClientBuilder.create().build();
         this.result = new StringBuilder();
         String auth = String.format("%s:%s", username, password);
         byte[] encodedAuth = Base64.encodeBase64(
                 auth.getBytes(StandardCharsets.ISO_8859_1));
-        authHeader = "Basic " + new String(encodedAuth);
+        this.authHeader = "Basic " + new String(encodedAuth);
     }
 
     public boolean setOn(int index) {
-        if (checkIndex(index)) {
+        if (!checkIndex(index)) {
             return false;
         }
         String command = String.format("http://%s/outlet?%d=ON", this.host, index);
@@ -44,7 +41,7 @@ public class PowerSwitch {
     }
 
     public boolean setOff(int index) {
-        if (checkIndex(index)) {
+        if (!checkIndex(index)) {
             return false;
         }
         String command = String.format("http://%s/outlet?%d=OFF", this.host, index);
@@ -56,18 +53,19 @@ public class PowerSwitch {
         String command = String.format("http://%s/outlet?a=ON", this.host);
         return powerSwitch(command) == 200;
     }
+    
 
-    public boolean setCycle(int index) {
-        if (checkIndex(index)) {
-            return false;
-        }
-        String command = String.format("http://%s/outlet?%d=CCL", this.host, index);
+    public boolean setOffAll() {
+        String command = String.format("http://%s/outlet?a=OFF", this.host);
         addLog(command);
         return powerSwitch(command) == 200;
     }
 
-    private boolean setOffAll() {
-        String command = String.format("http://%s/outlet?a=OFF", this.host);
+    public boolean setCycle(int index) {
+        if (!checkIndex(index)) {
+            return false;
+        }
+        String command = String.format("http://%s/outlet?%d=CCL", this.host, index);
         addLog(command);
         return powerSwitch(command) == 200;
     }
@@ -76,19 +74,19 @@ public class PowerSwitch {
         HttpGet request = new HttpGet(command);
         try {
             request.setHeader(HttpHeaders.AUTHORIZATION, authHeader);
-            HttpResponse response = httpclient.execute(request);
+            HttpResponse response = HttpClientBuilder.create().build().execute(request);
             addLog(response.getStatusLine().toString());
             return response.getStatusLine().getStatusCode();
         } catch (IOException e) {
             e.printStackTrace();
-            result.append("Exception: ").append(e.getMessage());
+            result.append("Exception: ").append(e.getLocalizedMessage());
             return -1;
         }
     }
 
     public String getResult() {
         String tempRs = result.toString();
-        result.delete(0, result.length());
+        result.setLength(0);
         return tempRs;
     }
 
@@ -99,9 +97,9 @@ public class PowerSwitch {
     private boolean checkIndex(int index) {
         if (index < 1 || index > 8) {
             result.append("index out of range!");
-            return true;
+            return false;
         }
-        return false;
+        return true;
     }
 
 }
